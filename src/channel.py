739a83061bcd4abbfbe3fdcb1fa6f786b54085ec
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-10 13:37:49 +0300

    change __init__

--- src/channel.py
+++ src/channel.py
@@ -14,11 +14,6 @@ class Channel:
         self.channel_id = channel_id
         self.api_key = os.environ.get('API_KEY')
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
-        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        self.playlist = self.youtube.playlists().list(channelId=channel_id,
-                                                      part='contentDetails,snippet',
-                                                      maxResults=50,
-                                                      ).execute()
         self.printout = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
