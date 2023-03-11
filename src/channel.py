c59f0fa072c6f26e4ccd8575844fd2e3777e8101
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-11 13:35:03 +0300

    add Channel argument

--- src/channel.py
+++ src/channel.py
@@ -6,13 +6,13 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
     api_key = os.environ.get('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """
         Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API.
         """
         self.channel_id = channel_id
-        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self) -> None:
         """
