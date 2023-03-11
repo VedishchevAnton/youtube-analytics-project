aee043be5e1a07cb6f56f8d7aa04ffc0d80c63f5
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-11 10:49:16 +0300

    change channel.py

--- src/channel.py
+++ src/channel.py
@@ -5,18 +5,18 @@ from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key = os.environ.get('API_KEY')
 
     def __init__(self, channel_id: str) -> None:
         """
         Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API.
         """
         self.channel_id = channel_id
-        self.api_key = os.environ.get('API_KEY')
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
-        self.printout = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """
         Выводит в консоль информацию о канале.
         """
-        print(json.dumps(self.printout, indent=2, ensure_ascii=False))
+        printout = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(printout, indent=2, ensure_ascii=False))
