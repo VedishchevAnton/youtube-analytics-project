299cba5cca9bb1b6fe87a032af022eef20df05cb
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-13 20:44:20 +0300

    внесены критические изменения

--- src/channel.py
+++ src/channel.py
@@ -12,22 +12,19 @@ class Channel:
         """
         Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API.
         """
-        self.__channel_id = channel_id
-        self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
-        self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
+        self.url = f'https://www.youtube.com/channel/{self.channel_id}'
 
     def print_info(self) -> None:
         """
         Выводит в консоль информацию о канале.
         """
-        printout = Channel.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        printout = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(printout, indent=2, ensure_ascii=False))
 
-    @property
-    def channel_id(self):
-        return self.__channel_id
 
     @classmethod
     def get_service(cls):
