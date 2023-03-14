a12682fc15e5c0d0452ad68c8f5099e0f8b86e0d
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-14 17:36:02 +0300

    добавлено кол-во подписчиков и общее кол-во просмотров в конструктор экземпляров

--- src/channel.py
+++ src/channel.py
@@ -16,6 +16,8 @@ class Channel:
         self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
         self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
 
     def print_info(self) -> None:
@@ -45,3 +47,4 @@ class Channel:
         """
         with open(file_name, 'w', encoding='utf-8') as file_json:
             json.dump(self.__dict__, file_json, ensure_ascii=False)
+
