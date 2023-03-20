26983f02f4dc527a373c633740b63d6b45924c64
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-20 22:24:54 +0300

    add class PLVideo

--- src/video.py
+++ src/video.py
@@ -18,3 +18,13 @@ class Video:
         self.link = f"https://www.youtube.com/watch?v={self.video_id}"
         self.view_count = self.video_response['items'][0]['statistics']['viewCount']
         self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class PLVideo(Video):
+
+    def __init__(self, video_id: str, playlist_id: str) -> None:
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
