b37307934ca97fdca122ad681c7b6a1a898ae8bd
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-27 21:24:52 +0300

    добавление исключений в __init__ class Video

--- src/video.py
+++ src/video.py
@@ -1,6 +1,7 @@
 import os
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
+from googleapiclient.errors import HttpError
 
 from src.setting import ENV_FILE
 
@@ -15,14 +16,23 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id: str) -> None:
-        self.video_id = video_id
-        self.video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                          id=video_id
-                                                          ).execute()
-        self.title = self.video_response['items'][0]['snippet']['title']
-        self.link = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            self.video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                              id=video_id
+                                                              ).execute()
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.link = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except HttpError:
+            print("non-existent id")
+        except IndexError:
+            self.video_response = None
+            self.title = None
+            self.link = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return f"{self.title}"
